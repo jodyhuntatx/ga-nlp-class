62aa0eb0e0b0703ffecc0fb476741756cda5dfe0
Ubuntu <ubuntu@ip-10-0-6-77.ca-central-1.compute.internal>
2024-07-30 16:19:36 +0000

    project work

--- setup/2-build-deps.sh
+++ setup/2-build-deps.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 main() {
-  build_llama_cpp
+  install_cuda_toolkit
+#  build_llama_cpp
 #  download_model_from_hf
+}
+
+#########################
+install_cuda_toolkit() {
+  wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+  sudo dpkg -i cuda-keyring_1.1-1_all.deb
+  sudo apt-get update
+  sudo apt-get -y install cuda
 }
 
 #########################
@@ -13,10 +22,12 @@ build_llama_cpp() {
 
 #########################
 download_model_from_hf() {
-  model_url="https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main"
-  model_name="llama-2-13b-chat.Q5_K_M.gguf"
+#  model_url="https://huggingface.co/TheBloke/Llama-2-13B-chat-GGUF/resolve/main"
+#  model_name="llama-2-13b-chat.Q5_K_M.gguf"
+  model_url="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main"
+  model_name="mistral-7b-instruct-v0.2.Q5_K_M.gguf"
   wget $model_url/$model_name
-  mv $model_name ..
+  mv $model_name ~/models
 }
 
 main "$@"
--- setup/3-run-jupyter.sh
+++ setup/3-run-jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ..
 echo "Detach: 'Ctrl-b d'"
 echo "Attach: 'tmux attach -t jupyter"
 read -n 1 -s -r -p "Press any key to continue"
+cd ~
 tmux new -s jupyter jupyter notebook --no-browser --port=3000 --ServerApp.ip=0.0.0.0
